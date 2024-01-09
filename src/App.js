bef94703eb4066a191b03515c85219a71ef9fb26
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-09 19:30:49 +0300

    Add path of the new Intro page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import SplashScreen from "./app/SplashScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Intro from "./app/Intro";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
 				}}
 			>
 				<Stack.Screen name="Home" component={SplashScreen} />
+				<Stack.Screen name="Intro Pages" component={Intro} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
