2130ebf15637bac5e74c92154bd48a2efcc0e930
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-09 17:11:09 +0300

    Feat flexible button mainly used for continue

---
+++ src/components/common/ContinueButton.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, Text, Pressable } from "react-native";
+
+export default function ContinueButton({ label, onPressFunc, extraStyles }) {
+	return (
+		<Pressable style={[styles.Button, extraStyles]} onPress={onPressFunc}>
+			<Text style={styles.Text}>{label}</Text>
+		</Pressable>
+	);
+}
+
+const styles = StyleSheet.create({
+	Button: {
+		width: "100%",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		boxSizing: "border-box",
+		backgroundColor: "rgba(227,86,42,1)",
+		borderRadius: 16,
+		padding: 16,
+	},
+	Text: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		color: "rgba(255,255,255,1)",
+		fontSize: 20,
+		fontWeight: "500",
+		textAlign: "center",
+	},
+});
