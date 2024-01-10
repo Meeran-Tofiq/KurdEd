d13ceb30f96855150427f518e52ad022b0c12742
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-10 20:45:08 +0300

    Feat footer with logic

--- src/app/CoursesScreen.js
+++ src/app/CoursesScreen.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Image, Text, FlatList, StyleSheet } from "react-native";
 import CourseCard from "../components/CourseCard";
 import courses from "../tests/__mocks__/courses";
 import WelcomeHeader from "../components/WelcomeHeader";
 import SearchBar from "../components/SearchBar";
+import Footer from "../components/Footer";
 
-export default function CoursesScreen() {
+export default function CoursesScreen({ navigation }) {
+	const [selectedScreen, setSelectedScreen] = useState(1);
 	const hasSubscribed = true;
 
 	return (
@@ -17,13 +19,22 @@ export default function CoursesScreen() {
 			<View>
 				<Text>وانەی پێشنیار کراو :</Text>
 			</View>
-			<FlatList
-				renderItem={({ item: course }) => (
-					<CourseCard course={course} hasSubscribed={hasSubscribed} />
-				)}
-				data={courses}
-				style={styles.flatlist}
-			/>
+			<View>
+				<FlatList
+					renderItem={({ item: course }) => (
+						<CourseCard course={course} hasSubscribed={hasSubscribed} />
+					)}
+					data={courses}
+					style={styles.flatlist}
+				/>
+			</View>
+			<View>
+				<Footer
+					navigation={navigation}
+					selectedScreen={selectedScreen}
+					setSelectedScreen={setSelectedScreen}
+				/>
+			</View>
 		</View>
 	);
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,60 @@
 import React from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function Footer({ navigation }) {
+export default function Footer({
+	navigation,
+	selectedScreen,
+	setSelectedScreen,
+}) {
+	const handlePress = (num) => {
+		setSelectedScreen(num);
+	};
+
 	return (
 		<View style={styles.footer}>
-			<Pressable style={styles.classesContainer}>
-				<Text style={styles.classes}>وانەکان</Text>
+			<Pressable style={styles.classesContainer} onPress={() => handlePress(1)}>
+				<Text style={selectedScreen === 1 ? styles.selectedText : styles.text}>
+					وانەکان
+				</Text>
 			</Pressable>
-			<Pressable style={styles.profileContainer}>
-				<Text style={styles.profile}>پڕۆفایل</Text>
+			<Pressable style={styles.profileContainer} onPress={() => handlePress(2)}>
+				<Text style={selectedScreen === 2 ? styles.selectedText : styles.text}>
+					پڕۆفایل
+				</Text>
 			</Pressable>
-			<Pressable style={styles.settingsContainer}>
-				<Text style={styles.settings}>ڕێکبەندییەکان</Text>
+			<Pressable
+				style={styles.settingsContainer}
+				onPress={() => handlePress(3)}
+			>
+				<Text style={selectedScreen === 3 ? styles.selectedText : styles.text}>
+					ڕێکبەندییەکان
+				</Text>
 			</Pressable>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	footer: {},
-	classesContainer: {},
-	classes: {},
-	profileContainer: {},
-	profile: {},
-	settingsContainer: {},
-	settings: {},
+	footer: {
+		position: "absolute",
+		bottom: 0,
+		flexDirection: "row-reverse",
+		height: 102,
+		width: "100%",
+		borderRadius: 12,
+		borderWidth: 1,
+		borderColor: "grey",
+		paddingTop: 12,
+		justifyContent: "space-around",
+		alignItems: "center",
+	},
+	text: {
+		fontSize: 16,
+		color: "rgba(190, 186, 179, 1)",
+	},
+	selectedText: {
+		fontSize: 16,
+		color: "#E3562A",
+	},
+	container: {},
 });
