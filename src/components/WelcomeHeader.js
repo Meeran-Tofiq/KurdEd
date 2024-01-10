e69808b7605bfbb5aaf5ac17a02a99c10bcb5ffb
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-10 18:51:06 +0300

    Feat component for greeting users

--- src/components/WelcomeHeader.js
+++ src/components/WelcomeHeader.js
@@ -1,4 +1,23 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
 
-export default function WelcomeHeader() {}
+export default function WelcomeHeader({ username }) {
+	return (
+		<View style={styles.welcomeHeader}>
+			<View style={styles.greetingContainer}>
+				<Text style={styles.greetingContainer}>سڵاو،</Text>
+			</View>
+			<View style={styles.usernameContainer}>
+				<Text style={styles.username}>{username}</Text>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	welcomeHeader: {},
+	greetingContainer: {},
+	usernameContainer: {},
+	greeting: {},
+	username: {},
+});
