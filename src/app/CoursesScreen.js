9b9419c8014d5eee5b9a4427b2beafe6309bc773
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-10 17:18:34 +0300

    Feat rendering courses on the screen

---
+++ src/app/CoursesScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Image, Text, FlatList, StyleSheet } from "react-native";
+import CourseCard from "../components/CourseCard";
+import courses from "../tests/__mocks__/courses";
+
+export default function CoursesScreen() {
+	const hasSubscribed = true;
+
+	return (
+		<View>
+			<FlatList
+				renderItem={({ item: course }) => (
+					<CourseCard course={course} hasSubscribed={hasSubscribed} />
+				)}
+				data={courses}
+				style={styles.flatlist}
+			/>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	flatlist: {
+		width: "auto",
+		height: 700,
+	},
+});
