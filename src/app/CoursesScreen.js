7e92758674b70426686594ac490af53efa4afee4
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-10 19:03:04 +0300

    Feat welcome header in the render

--- src/app/CoursesScreen.js
+++ src/app/CoursesScreen.js
@@ -2,12 +2,14 @@ import React from "react";
 import { View, Image, Text, FlatList, StyleSheet } from "react-native";
 import CourseCard from "../components/CourseCard";
 import courses from "../tests/__mocks__/courses";
+import WelcomeHeader from "../components/WelcomeHeader";
 
 export default function CoursesScreen() {
 	const hasSubscribed = true;
 
 	return (
 		<View>
+			<WelcomeHeader />
 			<FlatList
 				renderItem={({ item: course }) => (
 					<CourseCard course={course} hasSubscribed={hasSubscribed} />
