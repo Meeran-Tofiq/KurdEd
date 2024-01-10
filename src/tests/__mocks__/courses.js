8c3fbbb1f808f820c4beb73600bb968aae6b7e58
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-10 16:41:59 +0300

    Feat mocks for course info

---
+++ src/tests/__mocks__/courses.js
@@ -0,0 +1,41 @@
+import courseImage from "../../../assets/course-image-1.png";
+
+/**
+ * @typedef {Object} course
+ * @property {string} title
+ * @property {string} desc
+ * @property {object} duration
+ * @property {object} img
+ */
+
+const courses = [
+	{
+		title: "بیرکاری - مامۆستا ئەحمەد",
+		desc: "داتاشراوە و تەواوکاری",
+		duration: {
+			hours: 10,
+			minutes: 34,
+		},
+		img: img,
+	},
+	{
+		title: "بیرکاری - مامۆستا ئەحمەد",
+		desc: "داتاشراوە و تەواوکاری",
+		duration: {
+			hours: 10,
+			minutes: 34,
+		},
+		img: img,
+	},
+	{
+		title: "بیرکاری - مامۆستا ئەحمەد",
+		desc: "داتاشراوە و تەواوکاری",
+		duration: {
+			hours: 10,
+			minutes: 34,
+		},
+		img: img,
+	},
+];
+
+export default courses;
