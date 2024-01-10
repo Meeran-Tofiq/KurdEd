99aa7eaa1f209aacf6da71b26c70a597076b9ed8
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-10 18:53:38 +0300

    Feat handling of unregistered users

--- src/components/WelcomeHeader.js
+++ src/components/WelcomeHeader.js
@@ -5,10 +5,10 @@ export default function WelcomeHeader({ username }) {
 	return (
 		<View style={styles.welcomeHeader}>
 			<View style={styles.greetingContainer}>
-				<Text style={styles.greetingContainer}>سڵاو،</Text>
+				<Text style={styles.greetingContainer}>{username ? "سڵاو،" : ""}</Text>
 			</View>
 			<View style={styles.usernameContainer}>
-				<Text style={styles.username}>{username}</Text>
+				<Text style={styles.username}>{username || "بەخێربێیت"}</Text>
 			</View>
 		</View>
 	);
