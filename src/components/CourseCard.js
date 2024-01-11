a9f3a5b1a75f33f732513d4681751dbc6371cd23
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-11 14:59:10 +0300

    Style info text of courses

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -18,28 +18,62 @@ import { View, Image, Text, StyleSheet } from "react-native";
  */
 export default function CourseCard({ course, hasSubscribed }) {
 	return (
-		<View>
+		<View style={styles.courseCard}>
 			<View
 				style={
 					hasSubscribed ? styles.imageContainer : styles.lockedImageContainer
 				}
 			>
 				{/* Fotn Awesome Lock icon goes here */}
-				<Image source={course.img} />
+				<Image source={course.img} style={styles.courseImage} />
 			</View>
-			<View>
-				<Text>
+			<View style={styles.courseInfoContainer}>
+				<Text style={styles.durationText}>
 					{course.duration.hours} کاتژمێر و {course.duration.minutes} خولەک
 				</Text>
-				<Text>{course.title}</Text>
-				<Text>{course.desc}</Text>
+				<Text style={styles.titleText}>{course.title}</Text>
+				<Text style={styles.descText}>{course.desc}</Text>
 			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	lockedImageContainer: {},
-	imageContainer: {},
-	courseInfoContainer: {},
+	courseCard: {
+		borderWidth: 1,
+		borderColor: "grey",
+		borderRadius: 15,
+		marginVertical: 15,
+	},
+	courseImage: {
+		width: "100%",
+		height: 180,
+	},
+	lockedImageContainer: {
+		padding: 15,
+		color: "#F8F2EE",
+	},
+	imageContainer: {
+		padding: 15,
+		color: "#F8F2EE",
+	},
+	courseInfoContainer: {
+		padding: 10,
+		backgroundColor: "white",
+		borderBottomLeftRadius: 15,
+		borderBottomRightRadius: 15,
+		borderBottomColor: "grey",
+		borderBottomWidth: 0.25,
+		alignItems: "flex-end",
+	},
+	durationText: {
+		color: "#5BA092",
+		fontSize: 12,
+	},
+	titleText: {
+		fontSize: 22,
+	},
+	descText: {
+		color: "grey",
+	},
 });
