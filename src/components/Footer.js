07c028f30ba18ec5cb6366643ff7c180275d7022
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-11 16:36:24 +0300

    merge with prev

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function Footer({
-	navigation,
-	selectedScreen,
-	setSelectedScreen,
-}) {
+export default function Footer({ navigation, selectedScreen }) {
 	const pages = ["Courses", "Profile Screen", "Settings Screen"];
 
 	const handlePress = (num) => {
