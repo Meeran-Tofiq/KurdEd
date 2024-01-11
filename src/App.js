ebd5c90d0e0ab85332fe6570dab9077eb6e5c5c5
MohammedF02 <121065388+MohammedF02@users.noreply.github.com>
2024-01-12 00:48:17 +0300

    Feat: Add registration page navigation to App.js

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import IntroScreen from "./app/IntroScreen";
 import CoursesScreen from "./app/CoursesScreen";
 import LoginScreen from "./app/LoginScreen";
+import RegistrationScreen from "./app/RegistrationScreen";
 import ProfileScreen from "./app/ProfileScreen";
 import SettingsScreen from "./app/SettingsScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				screenOptions={{
-					headerShown: false,
-				}}
-			>
-				<Stack.Screen name="Splash Screen" component={SplashScreen} />
-				<Stack.Screen name="Intro Pages" component={IntroScreen} />
-				<Stack.Screen name="Courses" component={CoursesScreen} />
-				<Stack.Screen name="Login Page" component={LoginScreen} />
-				<Stack.Screen name="Profile Screen" component={ProfileScreen} />
-				<Stack.Screen name="Settings Screen" component={SettingsScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Splash Screen" component={SplashScreen} />
+        <Stack.Screen name="Intro Pages" component={IntroScreen} />
+        <Stack.Screen name="Courses" component={CoursesScreen} />
+        <Stack.Screen name="Login Page" component={LoginScreen} />
+        <Stack.Screen name="Registration Page" component={RegistrationScreen} />
+        <Stack.Screen name="Profile Screen" component={ProfileScreen} />
+        <Stack.Screen name="Settings Screen" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
