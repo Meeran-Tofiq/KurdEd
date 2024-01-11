edfa2bcc5a95ca51642f4718cab2b38936f07b31
MohammedF02 <121065388+MohammedF02@users.noreply.github.com>
2024-01-11 20:59:08 +0300

    Feat: Add login page to the app

--- src/app/LoginScreen.js
+++ src/app/LoginScreen.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Image, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Image, Text, StyleSheet, TextInput } from "react-native";
 import SkipButton from "../components/SkipButton";
 import woman from "../../assets/kurdish-woman-holding-book.png";
 import facebookLogo from "../../assets/facebook-logo.png";
@@ -7,13 +7,16 @@ import instagramLogo from "../../assets/instagram-logo.png";
 import googleLogo from "../../assets/google-logo.png";
 import loginLogo from "../../assets/login-logo.png";
 import alternateLoginLogo from "../../assets/alternate-login-text.png";
+import TextField from "../components/TextField";
+import ContinueButton from "../components/ContinueButton";
 
 export default function LoginScreen({ navigation }) {
-    const [email, setEmail] = useState('');
-
-    const handleEmailText = (inputEmail) => {
-        setEmail(inputEmail);
-    };
+    const handleLoginButton = () => {
+        navigation.navigate("Courses");
+	};
+    const handleRegisterButton = () => {
+        navigation.navigate("Registration Page");
+	};
 
     return (
         <View style={styles.container}>
@@ -23,13 +26,28 @@ export default function LoginScreen({ navigation }) {
             </View>
 
             <Image source={loginLogo} style={styles.loginLogo} />
-            <Image source={alternateLoginLogo} style={{resizeMode: "contain"}}/>
+            <Image source={alternateLoginLogo} style={{ resizeMode: "contain", alignSelf: "center" }} />
 
             <View style={styles.loginIconsContainer}>
-                <Image source={facebookLogo} style={styles.loginIcon}/>
-                <Image source={instagramLogo} style={styles.loginIcon}/>
-                <Image source={googleLogo} style={styles.loginIcon}/>
+                <Image source={facebookLogo} style={styles.loginIcon} />
+                <Image source={instagramLogo} style={styles.loginIcon} />
+                <Image source={googleLogo} style={styles.loginIcon} />
             </View>
+            <View style={styles.inputContainer}>
+                <TextField placeholder={"ئیمەیڵ"} autoCapitalize={"none"} extraStyles={styles.emailField}/>
+                <TextField secureTextEntry={true} placeholder={"پاسوۆرد"} autoCapitalize={"none"} />
+            </View>
+            <Text style={{alignSelf: "center", marginTop: 16}}>
+            پاسوۆردت لە بیرکردووە؟
+            </Text>
+            <ContinueButton
+            label={"چوونە ژوورەرە"}
+            extraStyles={styles.loginButton}
+            onPressFunc={handleLoginButton}
+            />
+            <Text style={{alignSelf: "center", marginTop: 16}} onPress={handleRegisterButton}>
+                خۆ تۆمارکردن
+            </Text>
         </View>
     )
 }
@@ -38,12 +56,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
     },
     womanImage: {
         width: 215,
         height: 230,
-        marginTop: -330, 
+        marginTop: 0,
+        alignSelf: "center"
     },
     skipButton: {
         top: 40,
@@ -54,15 +72,27 @@ const styles = StyleSheet.create({
         borderRadius: 20,
     },
     loginLogo: {
-        marginTop: -80,
+        alignSelf: "center"
     },
     loginIconsContainer: {
         display: "flex",
         flexDirection: "row",
         gap: 24,
+        marginTop: 10,
+        alignSelf: "center"
     },
     loginIcon: {
         width: 48,
         height: 48
+    },
+    inputContainer: {
+        gap: 16
+    },
+    emailField: {
+        marginTop: 16
+    },
+    loginButton: {
+        alignSelf: "center",
+        marginTop: 16
     }
 })
