436dc97c10b4b86546317136b0fbc7a3d3b9d2a2
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-11 20:35:32 +0300

    Fix error, text was written outside of Text comp

--- src/components/ProfileRedirectButton.js
+++ src/components/ProfileRedirectButton.js
@@ -11,7 +11,7 @@ export default function ProfileRedirectButton({
 			style={styles.redirectBtn}
 			onPress={() => navigation.navigate(redirectLink)}
 		>
-			{label}
+			<Text>{label}</Text>
 		</Pressable>
 	);
 }
