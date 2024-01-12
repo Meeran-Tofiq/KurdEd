323c99ed744bad25b75874ef3ae43a05570b4337
MohammedF02 <121065388+MohammedF02@users.noreply.github.com>
2024-01-12 22:13:56 +0300

    Fix: Fix the subscription state on the courses screen

--- src/app/CoursesScreen.js
+++ src/app/CoursesScreen.js
@@ -1,65 +1,65 @@
 import React, { useState } from "react";
-import { View, Image, Text, FlatList, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 import CourseCard from "../components/CourseCard";
 import courses from "../tests/__mocks__/courses";
 import WelcomeHeader from "../components/WelcomeHeader";
 import SearchBar from "../components/SearchBar";
 import Footer from "../components/Footer";
 
-export default function CoursesScreen({ navigation }) {
-	const [filteredCourses, setFilteredCourses] = useState(courses);
-	const hasSubscribed = true;
+export default function CoursesScreen({ route, navigation }) {
+  const [filteredCourses, setFilteredCourses] = useState(courses);
+  const hasSubscribed = route.params?.hasSubscribed ?? false;
 
-	return (
-		<View>
-			<View style={styles.content}>
-				<View style={styles.header}>
-					<View style={styles.headerContainer}>
-						<WelcomeHeader />
-					</View>
-					<SearchBar
-						filteredCourses={filteredCourses}
-						setFilteredCourses={setFilteredCourses}
-					/>
-					<Text style={styles.suggestionText}>وانەی پێشنیار کراو:</Text>
-				</View>
-				<FlatList
-					renderItem={({ item: course }) => (
-						<CourseCard course={course} hasSubscribed={hasSubscribed} />
-					)}
-					data={filteredCourses}
-					style={styles.flatlist}
-				/>
-			</View>
-			<View>
-				<Footer navigation={navigation} selectedScreen={1} />
-			</View>
-		</View>
-	);
+  return (
+    <View>
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <View style={styles.headerContainer}>
+            <WelcomeHeader />
+          </View>
+          <SearchBar
+            filteredCourses={filteredCourses}
+            setFilteredCourses={setFilteredCourses}
+          />
+          <Text style={styles.suggestionText}>وانەی پێشنیار کراو:</Text>
+        </View>
+        <FlatList
+          renderItem={({ item: course }) => (
+            <CourseCard course={course} hasSubscribed={hasSubscribed} />
+          )}
+          data={filteredCourses}
+          style={styles.flatlist}
+        />
+      </View>
+      <View>
+        <Footer navigation={navigation} selectedScreen={1} />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	content: {
-		height: "100%",
-		padding: 15,
-		alignItems: "center",
-		paddingBottom: 75,
-	},
-	header: {
-		height: "25%",
-		width: "100%",
-		justifyContent: "flex-end",
-		gap: 15,
-	},
-	headerContainer: {
-		width: "100%",
-		flexDirection: "row-reverse",
-		alignItems: "center",
-	},
-	suggestionText: {
-		textAlign: "center",
-	},
-	flatlist: {
-		width: "100%",
-	},
+  content: {
+    height: "100%",
+    padding: 15,
+    alignItems: "center",
+    paddingBottom: 75,
+  },
+  header: {
+    height: "25%",
+    width: "100%",
+    justifyContent: "flex-end",
+    gap: 15,
+  },
+  headerContainer: {
+    width: "100%",
+    flexDirection: "row-reverse",
+    alignItems: "center",
+  },
+  suggestionText: {
+    textAlign: "center",
+  },
+  flatlist: {
+    width: "100%",
+  },
 });
