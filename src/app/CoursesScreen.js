178f6b483cd041e8f569d1f3d52da7f4ac205272
MohammedF02 <121065388+MohammedF02@users.noreply.github.com>
2024-01-13 23:36:09 +0300

    Refactor: Courses can be pressed and navigate to their details

--- src/app/CoursesScreen.js
+++ src/app/CoursesScreen.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import CourseCard from "../components/CourseCard";
 import graduationImage from "../../assets/graduation-hat-small.png";
 import courses from "../tests/__mocks__/courses";
@@ -9,67 +15,73 @@ import Footer from "../components/Footer";
 import CircularButton from "../components/CircularButton";
 
 export default function CoursesScreen({ navigation }) {
-	const [filteredCourses, setFilteredCourses] = useState(courses);
-	const hasSubscribed = true;
+  const [filteredCourses, setFilteredCourses] = useState(courses);
+  const hasSubscribed = true;
 
-	return (
-		<View>
-			<View style={styles.content}>
-				<View style={styles.header}>
-					<View style={styles.headerContainer}>
-						<WelcomeHeader />
-						<CircularButton
-							image={graduationImage}
-							onPressFunc={() => {
-								navigation.navigate("Year Selection Screen");
-							}}
-						/>
-					</View>
-					<SearchBar
-						filteredCourses={filteredCourses}
-						setFilteredCourses={setFilteredCourses}
-					/>
-					<Text style={styles.suggestionText}>وانەی پێشنیار کراو:</Text>
-				</View>
-				<FlatList
-					renderItem={({ item: course }) => (
-						<CourseCard course={course} hasSubscribed={hasSubscribed} />
-					)}
-					data={filteredCourses}
-					style={styles.flatlist}
-				/>
-			</View>
-			<View>
-				<Footer navigation={navigation} selectedScreen={1} />
-			</View>
-		</View>
-	);
+  const handleCoursePress = (course) => {
+    navigation.navigate("Courses Detail Screen", { course });
+  };
+
+  return (
+    <View>
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <View style={styles.headerContainer}>
+            <WelcomeHeader />
+            <CircularButton
+              image={graduationImage}
+              onPressFunc={() => {
+                navigation.navigate("Year Selection Screen");
+              }}
+            />
+          </View>
+          <SearchBar
+            filteredCourses={filteredCourses}
+            setFilteredCourses={setFilteredCourses}
+          />
+          <Text style={styles.suggestionText}>وانەی پێشنیار کراو:</Text>
+        </View>
+        <FlatList
+          renderItem={({ item: course }) => (
+            <TouchableOpacity onPress={() => handleCoursePress(course)}>
+              <CourseCard course={course} hasSubscribed={hasSubscribed} />
+            </TouchableOpacity>
+          )}
+          data={filteredCourses}
+          style={styles.flatlist}
+        />
+      </View>
+      <View>
+        <Footer navigation={navigation} selectedScreen={1} />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	content: {
-		height: "100%",
-		padding: 15,
-		paddingTop: 25,
-		alignItems: "center",
-		paddingBottom: 75,
-	},
-	header: {
-		height: "25%",
-		width: "100%",
-		justifyContent: "flex-end",
-		gap: 15,
-	},
-	headerContainer: {
-		width: "100%",
-		flexDirection: "row-reverse",
-		alignItems: "center",
-		justifyContent: "space-between",
-	},
-	suggestionText: {
-		textAlign: "center",
-	},
-	flatlist: {
-		width: "100%",
-	},
+  content: {
+    height: "100%",
+    padding: 15,
+    paddingTop: 25,
+    alignItems: "center",
+    paddingBottom: 75,
+  },
+  header: {
+    height: "25%",
+    width: "100%",
+    justifyContent: "flex-end",
+    gap: 15,
+  },
+  headerContainer: {
+    width: "100%",
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  suggestionText: {
+    textAlign: "center",
+  },
+  flatlist: {
+    width: "100%",
+  },
 });
