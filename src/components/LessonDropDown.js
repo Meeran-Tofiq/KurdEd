a6e3fd9ebb51502bbeeb09483e69ec86488b33d8
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-13 16:36:18 +0300

    Feat basic import and export

---
+++ src/components/LessonDropDown.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default function LessonDropDown({ navigation }) {
+	return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create();
