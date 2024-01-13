49c117b82098f55c181d8c3d02a81674f692d817
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-13 17:24:34 +0300

    Feat display of lessons with clickable dropdown

--- src/components/LessonDropDown.js
+++ src/components/LessonDropDown.js
@@ -1,8 +1,32 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import LessonOption from "./LessonOption";
 
-export default function LessonDropDown({ navigation }) {
-	return <View style={styles.container}></View>;
+export default function LessonDropDown({ navigation, dropdownLabel, lessons }) {
+	const [isVisible, setIsVisible] = useState(false);
+	return (
+		<View style={styles.container}>
+			<Pressable
+				style={styles.dropdown}
+				onPress={() => setIsVisible(!isVisible)}
+			>
+				<Text>{dropdownLabel}</Text>
+			</Pressable>
+
+			<View
+				style={isVisible ? styles.lessons : [setyles.lesson, { height: 0 }]}
+			>
+				{lessons.map((lesson) => {
+					return (
+						<LessonOption
+							label={lesson.label}
+							onPressFunc={() => navigation.navigate(lesson.link)}
+						/>
+					);
+				})}
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create();
