6d947cb4a8de2e7b6d4fc8f54f4e799c51a2b10c
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-13 19:25:27 +0300

    Style flatlist to not stop before end of screen

--- src/app/YearSelectionScreen.js
+++ src/app/YearSelectionScreen.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
 		alignItems: "center",
 		width: "100%",
 		paddingTop: "25%",
-		paddingBottom: "25%",
+		paddingBottom: "10%",
 	},
 	header: {
 		width: "100%",
